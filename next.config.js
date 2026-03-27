9a50c29f3e8a8f5452fc448f88bd038f6c0b4df5
Tom Almog <tomalmog2@gmail.com>
2026-03-27 00:16:30 -0400

    Remove old rewrite to deleted Vercel project

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-      return [
-          {
-              source: '/bmm/:path*',
-              destination: 'https://benchmark-mondays-web.vercel.app/bmm/:path*',
-          },
-      ];
-  },
+    reactStrictMode: true,
 }
 
 module.exports = nextConfig
